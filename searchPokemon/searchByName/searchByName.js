f3c4278831312e2befac7bd9bb298b3fa36ce402
KushRohra <iec2017024@iiita.ac.in>
2020-10-19 15:38:45 +0530

    search by Name completed

--- searchPokemon/searchByName/searchByName.js
+++ searchPokemon/searchByName/searchByName.js
@@ -1,4 +1,4 @@
-let url = "https://pokeapi.co/api/v2/pokemon/"
+let url = "https://pokeapi.co/api/v2/pokemon/";
 
 var allPokemon = new Map()
 
@@ -16,7 +16,35 @@ async function getAllPokemon() {
 	}
 }
 
-function searchPokemon() {
+async function searchPokemon() {
 	var pokemonName = document.getElementById("name").value;
-	console.log(allPokemon.get(pokemonName.toLowerCase()))
+	var c = 0;
+	var pokemonResult = document.getElementById("pokemonResult");
+	var content = `<ul class="list-group">`;
+	for (let [key, value] of allPokemon) {
+		if(key.includes(pokemonName)) {
+			c++;
+			content += `<a class="btn" href="../../seePokemon/seePokemon.html?id=${await getId(value)}"><li>${key}</li></a>`;
+		}
+	}
+	content += `</ul>`;
+	if(c!=0) {
+		pokemonResult.innerHTML = content;
+	}
+	else {
+		pokemonResult.innerHTML = `No results found`;
+	}
+	document.getElementById("form").reset();
+}
+
+async function getId(url) {
+	url = url.toString();
+	var urlLength = url.length;
+	let no="";
+	for(var i=urlLength-2;i>=0;i--) {
+		if(url[i]>='0' && url[i]<='9')
+			no+=url[i];
+		else break;
+	}
+	return no.split('').reverse().join('');
 }
